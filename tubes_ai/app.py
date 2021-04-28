4bb61b6cdf61faa9ee8906da3b1a520bd0bfa723
Widi Afandi <54144923+technisekai@users.noreply.github.com>
2021-04-28 21:39:42 +0700

    Add files via upload

---
+++ tubes_ai/app.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, render_template, url_for, request
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'file_up/'
+ALLOWED_EXTENSIONS = {'txt'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# Halaman awal
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+	text_in = ''
+	berkas = ''
+	if request.method == 'POST':
+		try:
+			text_in = request.form['text-in']
+		except:
+			pass
+		
+		try:
+			berkas = request.files['file']
+		except:
+			pass
+		
+	if berkas and allowed_file(berkas.filename):
+			filename = secure_filename(berkas.filename)
+			berkas.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		
+	return render_template('index.html', text_in = text_in, berkas = berkas, judul='Home')
+    
+
+if __name__ == '__main__':
+	app.run(debug=True)
